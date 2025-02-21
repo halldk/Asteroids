8c876af0c8d8d68a49e0115f41282252a9d12c35
halldk <1168515+halldk@users.noreply.github.com>
2025-02-21 16:26:19 -0700

    Create sprite groups, add Player class

--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
-    
+
     def __init__(self, x:int, y:int):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.containers = ()
 
     # in the player class
     def triangle(self):
@@ -19,3 +20,22 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, 'white', self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            self.rotate((dt * -1))
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.rotate(dt)
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.move(dt)
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.move((dt * -1))
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
