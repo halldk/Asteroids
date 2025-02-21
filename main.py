05df683a3f9f3b8c08a6227b0764472236ab7e85
halldk <1168515+halldk@users.noreply.github.com>
2025-02-21 15:31:36 -0700

    Restrict to 60 FPS

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while(True):
         for event in pygame.event.get():
@@ -15,6 +17,7 @@ def main():
                 return
         screen.fill('black')
         pygame.display.flip()
+        dt = clock.tick(SCREEN_FPS) / 1000
 
 if __name__ == "__main__":
     main()
